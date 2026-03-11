6d6c43d2ff57b66794a29e027ff88f2840c4a80c
Litichevskiy Fedor <fedor.litichevskiy@gmail.com>
2026-03-11 22:00:07 +0300

    fix with import

--- src/ru/yandex/practicum/gym/Main.java
+++ src/ru/yandex/practicum/gym/Main.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.gym;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -1,6 +1,12 @@
 package ru.yandex.practicum.gym;
 
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.Collections;
+
 
 
 public class Timetable {
